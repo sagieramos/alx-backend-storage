06827a7ffa6acf90fb6b4e28370c214511f5fe31
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-05-09 21:51:22 +0100

    _

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,18 +3,19 @@
 '''
 from pymongo import MongoClient
 
+
 def print_nginx_request_logs(nginx_collection):
     '''Prints stats about Nginx request logs.
     '''
-    print('{} logs'.format(nginx_collection.count()))
+    print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = nginx_collection.count({'method': method})
+        req_count = len(list(nginx_collection.find({'method': method})))
         print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = nginx_collection.count({
-        'method': 'GET', 'path': '/status'
-    })
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
     print('{} status check'.format(status_checks_count))
 
 
